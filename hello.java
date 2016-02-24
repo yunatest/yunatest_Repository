f405fc2595b43ccb39b7abbd188a1db61a2eb72f
yunatest <yuna.cho@g-smattglobal.com>
2016-02-24 10:28:29 +0900

    all

--- hello.java
+++ hello.java
@@ -1,8 +1,6 @@
 class hello{
 	public static void main(String[] args)
 	{
-		System.out.println("Hello World\n");
-		System.out.println("Hello man\n");
-		System.out.println("Hello woman\n");
+		System.out.println("Hello World");
 	}
 }
